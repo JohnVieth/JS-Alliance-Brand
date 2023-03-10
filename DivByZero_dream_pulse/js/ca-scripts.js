d41b59cd2855d4948a28fad6fff9a29ce92d8bab
John Vieth <john.vieth@pbswisconsin.org>
2023-03-10 00:07:13 -0600

    Finished assignment

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -11,18 +11,34 @@ function addCustomerName() {
     // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
 
 };
+addCustomerName()
 
 function toggleColors() {
     // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
+    let arrElem = document.querySelectorAll('.tm-white-rect')
+    arrElem.forEach(element => {
+        element.classList.remove('tm-white-rect')
+        element.classList.add('tm-blue-rect')
+    });
 
 };
+toggleColors()
 
 function hideElement() {
+    document.querySelector('.tm-social-links').style.display = "none";
     //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
 
 };
+hideElement()
 
  function addText() {
+
+    document.querySelector('h2#introheader').textContent = "Alliance"
+    document.querySelector('p#intropara1').textContent = "Alliance was founded in 1931 by Albert Daniels."
+    document.querySelector('p#intropara2').textContent = "The company is a leader in P.C. Manufacturing"
+    document.querySelector('p#aboutpara1').textContent = "We deliver the best service this side of creation"
+    document.querySelector('p#aboutpara2').textContent = "Our motto is satisfaction guaranteed"
+    document.querySelector('p#addresspara').textContent = "1-800-565-5656"
 
     //<p id="intropara1" class="tm-color-gray">  <!--todo: Add customer company introduction text about their founding year-->
 
@@ -37,4 +53,4 @@ function hideElement() {
 }; 
 
 
-
+addText()
